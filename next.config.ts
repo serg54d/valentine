c8e0a730fb9c6bbd337897c99e15fb56955c56f1
sadunaev <serg54d@bk.ru>
2026-02-13 23:09:25 +0300

    fix basePath for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    // Если репозиторий НЕ username.github.io, а например valentine,
-    // то раскомментируй и поставь имя репо:
-    // basePath: "/valentine",
-    // assetPrefix: "/valentine/",
+    basePath: "/valentine",
+    assetPrefix: "/valentine/",
     images: {
         unoptimized: true,
     },
